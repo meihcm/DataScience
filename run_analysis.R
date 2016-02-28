b5bcf9253ae6181be32a50875d6fdd20e5e46d7f
Mike <mchiem@gmail.com>
2016-02-28 09:12:53 -0500

    TEST_Y

--- run_analysis.R
+++ run_analysis.R
@@ -1,20 +1,20 @@
 ## SETUP
 #### Assumes default is UCI Data home
-PROJECT_HOME <- "/Users/michaelchiem/DataScience"
+PROJECT_HOME <- paste("~/DataScience") ##"/Users/michaelchiem/DataScience"
 DATASET_HOME <- paste(PROJECT_HOME,"/UCI HAR Dataset",sep="")
 TEST_HOME <- paste(PROJECT_HOME,"/UCI HAR Dataset/test",sep="")
 TRAIN_HOME <- paste(PROJECT_HOME,"/UCI HAR Dataset/train",sep="")
 
 setwd(DATASET_HOME)
 ACTIVITY_LABELS <- read.table("activity_labels.txt", col.names = c("activityKey","activityLabel"), header=FALSE)
-FEATURE_MAP <- read.table("features.txt", col.names = c("featureKey", "featureValues"), header=FALSE)
+FEATURE_MAP <- read.table("features.txt", header=FALSE)
 
 ## 1 Merge the training and test sets to create one dataset
 #### 1.1 LOAD ALL TEST FILES
 setwd(TEST_HOME)
 TEST_SUBJECT_ROWS <- read.table("subject_test.txt", col.names = c("subjectKey"), header=FALSE)
 TEST_Y_LABEL_ROWS <- read.table("y_test.txt", col.names=c("labelKey"), header=FALSE)
-TEST_Y_DATA <- read.table("X_test.txt", col.names=c("labelKey"), header=FALSE)
+TEST_Y_DATA <- read.table("X_test.txt", col.names=as.vector(T_FEATURE_MAP$V2), header=FALSE)
 ## ?? What is TEST_Y_DATA vs the inertial data??
 
 #### 1.2 LOAD ALL TRAIN FILES
