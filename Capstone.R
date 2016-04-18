b89d998340251fb6de35130d850df14f3b612290
michaelc <michael.chiem@nbcuni.com>
2016-04-18 15:20:41 -0400

    test

--- Capstone.R
+++ Capstone.R
@@ -5,4 +5,12 @@ projectHome <- paste("~/DataScience") ##"/Users/michaelchiem/DataScience"
 datasetHome <- paste(projectHome,"/OnlineNewsPopularity",sep="")
 setwd(datasetHome)
 mashable_df = read.csv("OnlineNewsPopularity.csv", sep=",", header = TRUE)
+counter = 0;
+for(thisUrl in mashable_df$url) 
+{
+  print(thisUrl)
+  counter = counter + 1
+  if (counter > 10)
+    break
+}
 
