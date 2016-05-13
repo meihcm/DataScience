084c72095ddfab391fb39cc2bab7e1675783d3c9
Mike <mchiem@gmail.com>
2016-05-13 02:29:07 -0400

    made auc for random forest

--- Capstone_Analyze_RandomForest.R
+++ Capstone_Analyze_RandomForest.R
@@ -1,9 +1,6 @@
 library(randomForest)
 forest_train = randomForest(shares ~ 
-                              num_imgs + num_videos + data_channel_is_lifestyle + data_channel_is_entertainment 
-                            + data_channel_is_bus + data_channel_is_socmed + data_channel_is_tech 
-                            + data_channel_is_world + weekday_is_monday + weekday_is_tuesday + weekday_is_wednesday 
-                            + weekday_is_thursday + weekday_is_friday , data = training_df)
+                              LDA_00 + LDA_01 + LDA_02 + LDA_03 + global_subjectivity, data = training_df)
 print(forest_train)
 testforest = predict(forest_train, newdata=testing_df)
 table(testforest, testing_df$shares) #confusion matrix for test set
@@ -11,8 +8,10 @@ table(testforest, testing_df$shares) #confusion matrix for test set
 
 
 > #prepare model for ROC Curve
-test.forest = predict(forest_train, type = "prob", newdata = testing_df)
-forestpred = prediction(test.forest[,2], testing_df$shares)
+test.forest = predict(forest_train, type = "prob")
+forestpred = prediction(test.forest[,2], training_df$shares)
 forestperf = performance(forestpred, "tpr", "fpr")
+forestperfauc = performance(forestpred, measure="auc")
+forestperfauc@y.values[[1]]
 plot(forestperf, col=1, add=TRUE)
 legend(0.6, 0.6, c('rforest'), 1:3)
