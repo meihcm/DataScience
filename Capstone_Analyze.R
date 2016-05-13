542d74b22cfa40d1a7f36f8e61dc6e07a49931e2
Mike <mchiem@gmail.com>
2016-05-13 02:03:46 -0400

    Changed to simpler view

--- Capstone_Analyze.R
+++ Capstone_Analyze.R
@@ -1,4 +1,5 @@
 library(dplyr)
+install.packages("psych")
 library(psych)
 install.packages("ROCR")
 library(ROCR)
@@ -20,6 +21,7 @@ names(new_df1) = names(new_df)
 new_df2 <- read.table("mashable_engineered.tbl", header=FALSE, sep='^', na.strings="NA")
 names(new_df2) = names(new_df)
 new_df <- rbind(new_df,new_df1,new_df2)
+new_df <- new_df[new_df$shares < 3000,]
 
 ## Change fields from factor to character classes
 new_df$title <- as.character(new_df$title)
@@ -44,11 +46,10 @@ new_df = new_df[!is.na(new_df$title_sentiment),]
 new_df = new_df[!is.na(new_df$full_sentiment),]
 
 ## convert Shares to 1 or 0 for popular or not
-## Mean is 1300, median is 1200, since we want to relax definition of popularity, we will use median
-## Everything equal/above 1200 is 1, everything below is 0
+## We are taking the mean to be the split of 1 when it is higher than mean otherwise 0
 ## describe(mashable_df$shares)
-new_df$shares[new_df$shares < 1921] = 0
-new_df$shares[new_df$shares >= 1921] = 1
+new_df$shares[new_df$shares < 1200] = 0
+new_df$shares[new_df$shares >= 1200] = 1
 new_df$shares = as.factor(new_df$shares)
 
 ## Make factor of sentiment
@@ -94,57 +95,35 @@ testing_df = subset(new_df,split==FALSE)
 ## Logistic model
 model1 <- glm(shares ~ title_sentiment + para1_sentiment + para2_sentiment 
               + para3_sentiment + full_sentiment,family=binomial(link='logit'),data=training_df)
-## remove paragraph1
-model2 <- glm(shares ~ title_sentiment + para2_sentiment + para3_sentiment + full_sentiment,family=binomial(link='logit'),data=training_df)
-## remove full_sentiment
-model3 <- glm(shares ~ title_sentiment + para2_sentiment + para3_sentiment,family=binomial(link='logit'),data=training_df)
-## remove paragraph3
-model4 <- glm(shares ~ title_sentiment + para2_sentiment,family=binomial(link='logit'),data=training_df)
-## remove paragraph2
-model5 <- glm(shares ~ title_sentiment,family=binomial(link='logit'),data=training_df)
+summary(model1)
+model2 <- glm(shares ~ title_sentiment
+                ,family=binomial(link='logit'),data=training_df)
+summary(model2)
 
-model11 <- glm(shares ~ title_sentiment + para2_sentiment 
-               + data_channel_is_lifestyle + data_channel_is_entertainment 
-               + data_channel_is_bus + data_channel_is_socmed + data_channel_is_tech 
-               + data_channel_is_world,family=binomial(link='logit'),data=training_df)
-model12 <-glm(shares ~ 
-                num_imgs + num_videos + data_channel_is_lifestyle + data_channel_is_entertainment 
-              + data_channel_is_bus + data_channel_is_socmed + data_channel_is_tech 
-              + data_channel_is_world + weekday_is_monday + weekday_is_tuesday + weekday_is_wednesday 
-              + weekday_is_thursday + weekday_is_friday 
-                ,family=binomial(link="logit"), data=training_df)
-summary(model4)
+## Predict model 1
+p_model1 <- predict(model1, type="response")
+pr_model1 <- prediction(p_model1, training_df$shares)
+prf_model1 <- performance(pr_model1, measure = "tpr", x.measure = "fpr")
+plot(prf_model1, colorize = TRUE, print.cutoffs.at=seq(0,1,.01),text.adj = c(-0.2,1.7))
+## AUC
+auc_model1 <- performance(pr_model1, measure = "auc")
+auc_model1 <- auc_model1@y.values[[1]]
+auc_model1
 
-## Predict
-p <- predict(model4, newdata=testing_df, type="response")
-pr <- prediction(p, testing_df$shares)
-prf <- performance(pr, measure = "tpr", x.measure = "fpr")
-plot(prf)
+## Predict model 2
+p_model2 <- predict(model2, type="response")
+## Small threshold will allow for larger 1 errors
+summary(p_model2)
+table(training_df$shares, p_model2 > 0.22)
+pr_model2 <- prediction(p_model2, training_df$shares)
+prf_model2 <- performance(pr_model2, "tpr", "fpr")
+plot(prf_model2, colorize = TRUE, print.cutoffs.at=seq(0,1,.01),text.adj = c(-0.2,1.7))
+## AUC
+auc_model2 <- performance(pr_model2, measure = "auc")
+auc_model2 <- auc_model2@y.values[[1]]
+auc_model2
 
-auc <- performance(pr, measure = "auc")
-auc <- auc@y.values[[1]]
-auc
-table(pr$labels,testing_df$shares)
-
-
-## Find baseline model's ability to predict popular
-confusionMatrix(p,testing_df$shares)
-
-baseline = nrow(new_df[new_df$shares == 1,])/nrow(new_df)
-
-
-
-par(mar=c(5,5,2,2),xaxs = "i",yaxs = "i",cex.axis=1.3,cex.lab=1.4)
-# plotting the ROC curve
-plot(prf,col="black",lty=3, lwd=3)
-# calculating AUC
-auc <- performance(pr,"auc")
-# now converting S4 class to vector
-auc <- unlist(slot(auc, "y.values"))
-# adding min and max ROC AUC to the center of the plot
-minauc<-min(round(auc, digits = 2))
-maxauc<-max(round(auc, digits = 2))
-minauct <- paste(c("min(AUC)  = "),minauc,sep="")
-maxauct <- paste(c("max(AUC) = "),maxauc,sep="")
-legend(0.3,0.6,c(minauct,maxauct,"\n"),border="white",cex=1.7,box.col = "white")
-#
+## Comparison
+## Baseline number of 1s in testing / all testing rows
+baseline_testing_df = nrow(testing_df[testing_df$shares==1,]) / nrow(testing_df)
+baseline_training_df = nrow(training_df[training_df$shares==1,]) / nrow(training_df)
